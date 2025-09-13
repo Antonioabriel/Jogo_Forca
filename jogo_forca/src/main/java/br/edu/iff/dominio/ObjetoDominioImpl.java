74457ed893f4b0573bcb30e16e84d17fa4ba5fe1
mariamdsouza <111377072+mariamdsouza@users.noreply.github.com>
2025-09-13 12:12:55 -0300

    Classe Tema criada

--- jogo_forca/src/main/java/br/edu/iff/dominio/ObjetoDominioImpl.java
+++ jogo_forca/src/main/java/br/edu/iff/dominio/ObjetoDominioImpl.java
@@ -1,10 +1,10 @@
 package br.edu.iff.dominio;
 
-public abstract class ObjetoDominioImpl implements ObjetoDominio {
+public abstract class ObjetoDominiolmpl implements ObjetoDominio {
 
     private long id;
 
-    public ObjetoDominioImpl(long id) {
+    public ObjetoDominiolmpl(long id) {
         this.id = id;
     }
 
